31b669783776aaf58d3e0cf5a57a5511f156da91
severikupari1 <severikupari1@gmail.com>
2024-02-13 08:53:30 +0200

    [AVAPALJU-271] Added catch to tags api call to prevent errors if tags api is not responding

--- src/hooks/useGridRowData.js
+++ src/hooks/useGridRowData.js
@@ -4,26 +4,32 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useGridRowData = folder => {
-  const [rowData, setRowData] = useState(null);
-  const [fetchError, setFetchError] = useState(false);
-  const { t } = useTranslation();
+    const [rowData, setRowData] = useState(null);
+    const [fetchError, setFetchError] = useState(false);
+    const {t} = useTranslation();
     useEffect(() => {
         // Start both requests simultaneously
         Promise.all([
-            axios.get(`${config.apiUrlFolders}${folder || config.defaultFolder}/`, { withCredentials: false }),
-            axios.get(`${config.tagsApiUrl}/v2/tags/public/tag/crud`)
+            axios.get(`${ config.apiUrlFolders }${ folder || config.defaultFolder }/`, {withCredentials: false}),
+            axios.get(`${ config.tagsApiUrl }/v2/tags/public/tag/crud`).catch(error => {
+                console.error("Failed to fetch tags:", error);
+                return null; // Return null value indicating the request failed
+            })
         ])
             .then(([folderResponse, tagsResponse]) => {
                 const folderData = folderResponse.data;
-                const tagsData = tagsResponse.data;
                 
                 // If folder data is available, process it with the tags data
                 if (folderData.aineisto) {
                     folderData.aineisto.forEach(row => {
                         row.tags = {};
-                        const tagData = tagsData.find(tag => tag.fileName === row.tiedosto);
-                        if (tagData) {
-                            row.tags = tagData;
+                        // Process tags data only if it's available
+                        if (tagsResponse) {
+                            const tagsData = tagsResponse.data;
+                            const tagData = tagsData.find(tag => tag.fileName === row.tiedosto);
+                            if (tagData) {
+                                row.tags = tagData;
+                            }
                         }
                     });
                     // Set the processed data
@@ -40,11 +46,11 @@ export const useGridRowData = folder => {
                 setRowData([]);
             });
     }, [folder, t]);
-
-  return {
-    rowData,
-    setRowData,
-    fetchError,
-    setFetchError
-  }
+    
+    return {
+        rowData,
+        setRowData,
+        fetchError,
+        setFetchError
+    }
 };
